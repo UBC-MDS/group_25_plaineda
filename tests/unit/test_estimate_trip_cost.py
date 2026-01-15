1aa0344c92b118e981a8866d158d90f48fe256e6
Hugo Kwok <hugokwok0119@gmail.com>
2026-01-15 11:12:36 -0800

    debug tests

--- tests/unit/test_estimate_trip_cost.py
+++ tests/unit/test_estimate_trip_cost.py
@@ -11,21 +11,24 @@ from dsci_524_group_25.estimate_trip_cost import estimate_trip_cost
 
 def test_general_cost():
     """Test a valid, non-negative input."""
-    assert estimate_trip_cost(100, 2, 4) == 50, f"Expected {expected} but got {cost}"
+    assert estimate_trip_cost(100, 2, 4) == 50, f"Expected {50} but got {estimate_trip_cost(100, 2, 4)}"
 
 def test_long_haul_cost():
     """Test a long haul case."""
-    assert estimate_trip_cost(1000, 2, 4) == 575, f"Expected {expected} but got {cost}"
+    assert estimate_trip_cost(1000, 2, 4) == 575, f"Expected {575} but got {estimate_trip_cost(1000, 2, 4)}"
 
 def test_invalid_distnace():
+    """Test a invalid distance case."""    
     with pytest.raises(ValueError, match="Distance must be greater than 0 km."):
         estimate_trip_cost(-100, 2, 4)
 
 def test_invalid_fuel_price():
+    """Test a invalid fuel price case."""    
     with pytest.raises(ValueError, match="Fuel price must be greater than 0."):
         estimate_trip_cost(100, -2, 4)
 
 def test_invalid_efficiency():
+    """Test a invalid efficiency case."""    
     with pytest.raises(ValueError, match="Efficiency (km/L) must be greater than 0."):
         estimate_trip_cost(100, -2, 4)
         
