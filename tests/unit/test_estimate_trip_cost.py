b09917e87d8a78c5c526fee90d9baa32048f6496
Hugo Kwok <hugokwok0119@gmail.com>
2026-01-16 15:23:43 -0800

    fix typo

--- tests/unit/test_estimate_trip_cost.py
+++ tests/unit/test_estimate_trip_cost.py
@@ -17,18 +17,18 @@ def test_long_haul_cost():
     """Test a long haul case."""
     assert estimate_trip_cost(1000, 2, 4) == 575, f"Expected {575} but got {estimate_trip_cost(1000, 2, 4)}"
 
-def test_invalid_distnace():
-    """Test a invalid distance case."""    
+def test_invalid_distance():
+    """Test a invalid distance case."""
     with pytest.raises(ValueError, match="Distance must be greater than 0 km."):
         estimate_trip_cost(-100, 2, 4)
 
 def test_invalid_fuel_price():
-    """Test a invalid fuel price case."""    
+    """Test a invalid fuel price case."""
     with pytest.raises(ValueError, match="Fuel price must be greater than 0."):
         estimate_trip_cost(100, -2, 4)
 
 def test_invalid_efficiency():
-    """Test a invalid efficiency case."""    
+    """Test a invalid efficiency case."""
     with pytest.raises(ValueError, match="Efficiency must be greater than 0."):
         estimate_trip_cost(100, 2, -4)
         
