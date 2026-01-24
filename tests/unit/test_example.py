fa84ca9619801ad4e24e9d492d9e813038f95843
Hooman Esteki <67445158+hoomanesteki@users.noreply.github.com>
2026-01-23 23:33:36 -0800

    Clean up test_add_numbers function
    Removed unnecessary docstring from the test function.

--- tests/unit/test_example.py
+++ tests/unit/test_example.py
@@ -5,9 +5,6 @@ A test module that tests the example module.
 from travelpy.example import add_numbers
 
 def test_add_numbers():
-    """
-    Test that add_numbers works as expected.
-    """
     out = add_numbers(1, 2)
     expected_out = 3
-    assert  out == expected_out, f"Expected {expected_out} but got {out}"
+    assert out == expected_out, f"Expected {expected_out} but got {out}"
